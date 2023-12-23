6c0e9b8d20dd004b82adcef2deb20db37e0d9f16
Eoin Kenny <eoinmkenny@gmail.com>
2023-12-23 11:27:37 -0500

    Update carracing.py
    Change version to v0

--- Car Racing/games/carracing.py
+++ Car Racing/games/carracing.py
@@ -68,7 +68,7 @@ class RacingNet(nn.Module):
 
 class CarRacing(gym.Wrapper):
     def __init__(self, frame_skip=0, frame_stack=4):
-        self.env = gym.make("CarRacing-v1")
+        self.env = gym.make("CarRacing-v0")
         super().__init__(self.env)
 
         self.frame_skip = frame_skip
